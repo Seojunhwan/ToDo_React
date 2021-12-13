01e9033f23cad9740cd536e5c9495559edcda8a5
SeoJunhwan <seojunhwan@kakao.com>
2021-12-13 15:27:29 +0900

    install library

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "styled-components";
+import { lightTheme } from "./theme";
+import { RecoilRoot } from "recoil";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <ThemeProvider theme={lightTheme}>
+        <App />
+      </ThemeProvider>
+    </RecoilRoot>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
